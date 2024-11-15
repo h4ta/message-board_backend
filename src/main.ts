f01d7dc233be201acd124ab3d358b4a812b46c28
h4ta <hatahatahata1013@gmail.com>
2024-11-15 14:12:22 +0900

    ポート番号を変更

--- src/main.ts
+++ src/main.ts
@@ -3,10 +3,14 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const port = process.env.PORT || 3000;
+
   app.enableCors({
     origin: '*',
-    allowedHeaders: '*',
+    allowedHeaders: 'Origin, X-Requested-With, Content-Type, Accept',
   });
-  await app.listen(3000);
+
+  console.log(`listening on port ${port}`);
+  await app.listen(port);
 }
 bootstrap();
