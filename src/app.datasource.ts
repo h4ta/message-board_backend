13a7278b1bc64ed70b7b04521a03f451ed3b9c3d
h4ta <hatahatahata1013@gmail.com>
2024-11-15 14:45:43 +0900

    renderのポート番号を削除

--- src/app.datasource.ts
+++ src/app.datasource.ts
@@ -3,7 +3,7 @@ import { DataSource } from 'typeorm';
 const AppDataSource = new DataSource({
   type: 'postgres', // データベースの種別。今回はpostgresqlへの接続とします。
 
-  host: 'dpg-csrdkqbtq21c73d1pf20-a:5432',
+  host: 'dpg-csrdkqbtq21c73d1pf20-a',
 
   username: 'testdb_gqw3_user',
 
