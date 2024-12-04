eb9e6fb2fcd3f84e2699e82804c73cdc35b0b807
h4ta <hatahatahata1013@gmail.com>
2024-12-04 13:58:19 +0900

    DB設定をrenderのDBに変更

--- src/app.datasource.ts
+++ src/app.datasource.ts
@@ -3,13 +3,13 @@ import { DataSource } from 'typeorm';
 const AppDataSource = new DataSource({
   type: 'postgres', // データベースの種別。今回はpostgresqlへの接続とします。
 
-  host: 'localhost',
+  host: 'dpg-csrebbt6l47c73fd8sqg-a',
 
-  username: 'postgres',
+  username: 'testdb_dgiu_user',
 
-  password: 'postgres',
+  password: 'lMfYGpW8a0VHRKMHwssPKxcdTls4MgeW',
 
-  database: 'testdb',
+  database: 'testdb_dgiu',
 
   entities: ['src/entities/*.ts'], //  エンティティファイル（後述）配列
 
